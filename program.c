b5f3382292b4d863ba3d4183450e6a41a783cd12
shiva21A <131233107+shiva21A@users.noreply.github.com>
2024-10-08 16:27:31 +0530

    Create program.c

---
+++ program.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+   int N, i, j;
+
+printf("Enter the number of rows: ");
+scanf("%d", &N);
+
+   for (i = 1; i<= N; i++) {
+      for (j = 1; j <= N - i; j++) {
+printf(" ");
+      }
+      for (j = 1; j <= i; j++) {
+printf("%d ", j);
+      }
+printf("\n");
+   }
+
+   return 0;
+}
